ae2061f41cfc7e992ee351cacaf14741fe814eef
Godfrey Timba <Godfreyt@qode.healthcare>
2021-09-16 00:26:25 +0200

    added swagger

--- src/main/java/com/crfstech/MyRemote/MyRemoteApplication.java
+++ src/main/java/com/crfstech/MyRemote/MyRemoteApplication.java
@@ -4,14 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MyRemoteApplication implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry){
 		registry.addResourceHandler("/**")
 				.addResourceLocations("classpath:/static/");
+		registry.
+				addResourceHandler("/swagger-ui/**")
+				.addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("**/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyRemoteApplication.class, args);
--- src/main/java/com/crfstech/MyRemote/security/SecurityConfig.java
+++ src/main/java/com/crfstech/MyRemote/security/SecurityConfig.java
@@ -12,6 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 @Configuration
@@ -46,16 +54,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(bCryptEncoder);
     }
 
+    private static final List<String> AUTH_LIST = Arrays.asList(
+            "/swagger-resources/**",
+            "/swagger-ui.html**",
+            "/webjars/**",
+            "/swagger-ui/**",
+            "favicon.ico");
+    @Bean
+    public BasicAuthenticationEntryPoint swaggerAuthenticationEntryPoint() {
+        BasicAuthenticationEntryPoint entryPoint = new BasicAuthenticationEntryPoint();
+        entryPoint.setRealmName("Swagger Realm");
+        return entryPoint;
+    }
+    private class CustomRequestMatcher implements RequestMatcher {
+
+        private List<AntPathRequestMatcher> matchers;
+
+        private CustomRequestMatcher(List<String> matchers) {
+            this.matchers = matchers.stream().map(AntPathRequestMatcher::new).collect(Collectors.toList());
+        }
+
+        @Override
+        public boolean matches(HttpServletRequest request) {
+            return matchers.stream().anyMatch(a -> a.matches(request));
+        }
+
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .csrf().disable()    //Disabling CSRF as not using form based login
                 .authorizeRequests()
-                .antMatchers("/signup", "/login","/images/**").permitAll()
+                .antMatchers("/signup", "/login","/images/**","/swagger-ui/**"
+                ,"/swagger-resources/**","/v2/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling()
+                .defaultAuthenticationEntryPointFor(swaggerAuthenticationEntryPoint(), new CustomRequestMatcher(AUTH_LIST))
                 .authenticationEntryPoint(authenticationEntryPoint)
                 .and()
                 .sessionManagement()
